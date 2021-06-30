5178e575a53c9a9ec47cba8d19f540da91e71475
xudafeng <xudafeng@126.com>
2021-07-01 00:18:03 +0800

    fix: lint

--- src/components/editor/format-md.js
+++ src/components/editor/format-md.js
@@ -1,12 +1,10 @@
-const formatMD = md => {
-  return md
-    .filter(item => item.text !== '\n')
-    .map(item => {
-      if (item.text) {
-        item.text = item.text.replace(/\n[\n]+/g, '\n');
-      }
-      return item;
-    });
-};
+const formatMD = md => md
+  .filter(item => item.text !== '\n')
+  .map(item => {
+    if (item.text) {
+      item.text = item.text.replace(/\n[\n]+/g, '\n');
+    }
+    return item;
+  });
 
 export default formatMD;
